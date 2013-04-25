06f05f49b53c63feef9ecaaa12274814e2bf9e58
stevesan <stevenan@gmail.com>
2013-04-24 23:45:22 -0700

    all python stuff ported to unity

--- unity_flowjam/Assets/scripts/RhymeScorer.js
+++ unity_flowjam/Assets/scripts/RhymeScorer.js
@@ -158,10 +158,110 @@ function ScorePronuns( aPhos:String[], bPhos:String[] )
     return score;
 }
 
+function GetPronunsForWord(word:String)
+{
+    var pronuns = new List.<String[]>();
+    pronuns.Add( proDict[word] );
+
+    var i = 2;
+    while( true )
+    {
+        var key = word+"("+i+")";
+        if( proDict.ContainsKey(key) )
+            pronuns.Add( proDict[key] );
+        else
+            break;
+        i++;
+    }
+
+    return pronuns;
+}
+
+function ScoreWords(a:String, b:String)
+{
+    // find all variants
+    var aPros = GetPronunsForWord(a);
+    var bPros = GetPronunsForWord(b);
+
+    // get max score of all unique pairings
+    var maxScore = 0.0;
+    for( var aPro in aPros )
+    {
+        for( var bPro in bPros )
+        {
+            var score = ScorePronuns(aPro, bPro);
+            maxScore = Mathf.Max( score, maxScore );
+        }
+    }
+    return maxScore;
+}
+
 function TestScoreWords(a:String, b:String, expectedScore:float)
 {
-    var score = ScorePronuns(proDict[a], proDict[b]);
+    var score = ScoreWords(a, b);
     Utils.Assert(score == expectedScore);
+}
+
+function RunTestCases()
+{
+    TestScoreWords('book'    , 'look'     , 1.5);
+    TestScoreWords('fashion' , 'ration'   , 2.5);
+    TestScoreWords('state'   , 'invade'   , 1.0);
+    TestScoreWords('invade'  , 'blade'    , 1.5);
+    TestScoreWords('then'    , 'them'     , 0.0);
+    TestScoreWords('close'   , 'close'    , 1.5);
+    TestScoreWords('rose'    , 'close'    , 1.5);
+
+    TestScoreWords('rose', 'close', 1.5);
+
+    TestScoreWords('out', 'doubt', 1.5);
+
+    TestScoreWords('proceeding', 'leading', 2.5);
+
+    TestScoreWords('dry', 'why', 1.0);
+    TestScoreWords('try', 'why', 1.0);
+    TestScoreWords('throttle', 'bottle', 2.5);
+    TestScoreWords('rhyme', 'sublime', 1.5);
+    TestScoreWords('climb', 'sublime', 1.5);
+    TestScoreWords('bending', 'ending', 2.5);
+    TestScoreWords('venting', 'ending', 2.0);
+    TestScoreWords('cacophony', 'monopoly', 2.0);
+    TestScoreWords('broccoli', 'monopoly', 3.0);
+
+    TestScoreWords('bastion', 'ration', 2.0);
+    TestScoreWords('motion', 'ration', 1.0);
+    TestScoreWords('bottle', 'ration', 0.0);
+
+    TestScoreWords('green', 'fiend', 1.0);
+    TestScoreWords('friend', 'end', 1.5);
+    TestScoreWords('one', 'thumb', 0.0);
+
+    TestScoreWords('love', 'move', 0.0);
+    TestScoreWords('real', 'still', 0.0);
+    TestScoreWords('compromise', 'promise', 0.0);
+
+    TestScoreWords('hollow', 'bottle', 0.0);
+
+    TestScoreWords('rose'    , 'close(2)' , 1.5);
+    TestScoreWords('monk'    , 'flunk' , 1.5);
+    TestScoreWords('mend'    , 'bend' , 1.5);
+    TestScoreWords('med'    , 'bed' , 1.5);
+    TestScoreWords('met'    , 'bed' , 1.0);
+    TestScoreWords('well'    , 'hell' , 1.0);
+    TestScoreWords('tank'    , 'bang' , 1.0);
+
+    TestScoreWords('skull'    , 'skulk' , 1.0);
+    TestScoreWords('bulk'    , 'skulk' , 1.5);
+    TestScoreWords('fang'    , 'bank' , 1.0);
+    TestScoreWords('rank'    , 'bank' , 1.5);
+    TestScoreWords('scrunch'    , 'lunch' , 1.5);
+
+    // check multi-consonant codas
+    TestScoreWords('clasp'    , 'clasped' , 1.0);
+    TestScoreWords('clasp'    , 'rasp' , 1.5);
+    TestScoreWords('broccoli', 'properly', 2.0);
+    TestScoreWords('broccoli', 'locally', 2.0);
+    Debug.Log('-- Tests done --');
 }
 
 function Awake()
@@ -180,12 +280,8 @@ function Awake()
     for( i = 0; i < SYLLABIC_CONSONANTS.length; i++ )
         sylConSet.Add(SYLLABIC_CONSONANTS[i]);
 
-    var syls = Phos2Syls( proDict['monopoly'] );
-    Debug.Log(Syls2String(syls));
 
-    TestScoreWords( 'fashion', 'ration', 2.5 );
-    TestScoreWords( 'broccoli', 'monopoly', 3.0 );
-    TestScoreWords( 'cacophony', 'monopoly', 2.0 );
+    RunTestCases();
 }
 
 function Start()
